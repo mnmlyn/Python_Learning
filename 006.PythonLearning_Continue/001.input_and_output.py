c3a241a74da41b4cfe8a46a3562bca8a1559c18f
mnmlyn <mnmlyn@163.com>
2018-11-07 16:49:54 +0800

    基本的输入输出

---
+++ 006.PythonLearning_Continue/001.input_and_output.py
@@ -0,0 +1,17 @@
+# python3.7
+# 输出到屏幕，字符串使用逗号连接时，输出会自动加一个空格
+print("Hello")
+print("Hello","World"+"!I am here!",666)
+print(666)
+# 从键盘输入
+# input要求输入正确的python表达式
+# raw_input将所有输入当做字符串，在python3中没有了raw_input
+a = input("Please input something：")
+print("You have input:",a)
+# 指定输出之后不加换行符，而是end指定的符号
+print("11+22=",end="")
+print(11+22)
+print("11+22=",11+22)
+
+
+
