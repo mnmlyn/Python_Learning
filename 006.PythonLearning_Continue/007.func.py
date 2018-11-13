60c6c5ecbd5dd0260d827ee2c1eda22f97ba317c
mnmlyn <mnmlyn@163.com>
2018-11-13 19:53:41 +0800

    函数的用法

---
+++ 006.PythonLearning_Continue/007.func.py
@@ -0,0 +1,15 @@
+# python3.7
+# 函数
+# 一些内建函数可以直接使用
+print(abs(-100))
+print(max(1,2,3,4))
+print(int("123"))
+print(int(12.34))
+print(float("12.34"))
+print(str(155))
+print(str(15.55))
+print(bool(1))
+# 可以为变量赋值为函数名，类似函数指针
+func = abs
+print(func(-100))
+
